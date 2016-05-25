a8b951bf176ba67478cabff1cdc296dc778875cd
Koutarou Chikuba <miz404@gmail.com>
2016-05-25 23:39:13 +0900

    Use class format

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello</h1>, document.querySelector(".mainContainer"));
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Hello/>,
+  document.querySelector(".mainContainer")
+);
